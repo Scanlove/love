1d06724347005a336e04688d92a9ac092019b485
Scanlove <Scanlove665@gmail.com>
2024-11-11 12:25:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Obtener todos los elementos con la clase .photo
 const photos = document.querySelectorAll('.photo');
+const countdownNumber = document.querySelector('.countdown-number');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalPhoto = document.querySelector('.modal-photo');
+const modalCloseBtn = document.querySelector('.modal-close');
 
 // Función para observar la aparición de las fotos
 const observer = new IntersectionObserver((entries) => {
@@ -17,3 +21,33 @@ const observer = new IntersectionObserver((entries) => {
 photos.forEach((photo) => {
   observer.observe(photo);
 });
+
+// Función para el contador regresivo
+function countdown() {
+  let count = 10;
+  const countdownInterval = setInterval(() => {
+    countdownNumber.textContent = count;
+    count--;
+
+    if (count === -1) {
+      clearInterval(countdownInterval);
+      countdownNumber.textContent = '';
+    }
+  }, 1000);
+}
+
+// Función para abrir la imagen en modal
+photos.forEach((photo, index) => {
+  photo.addEventListener('click', () => {
+    modalPhoto.src = `foto${index + 1}.png`;
+    modalOverlay.style.display = 'flex';
+  });
+});
+
+// Función para cerrar el modal
+modalCloseBtn.addEventListener('click', () => {
+  modalOverlay.style.display = 'none';
+});
+
+// Iniciar el contador regresivo
+countdown();
