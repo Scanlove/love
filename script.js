e861597976bba9f45be76f31fa585574a45747b2
Scanlove <Scanlove665@gmail.com>
2024-11-11 13:13:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,29 @@ const photos = document.querySelectorAll('.photo');
 const modalOverlay = document.querySelector('.modal-overlay');
 const modalPhoto = document.querySelector('.modal-photo');
 const modalCloseBtn = document.querySelector('.modal-close');
+const heartsContainer = document.querySelector('.hearts-container');
+
+// Función para generar corazones
+function createHearts() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  heart.style.left = `${Math.random() * 100}vw`;
+  heart.style.animationDuration = `${Math.random() * 2 + 3}s`; // Duración aleatoria para cada corazón
+  heart.innerText = '❤';
+  heartsContainer.appendChild(heart);
+
+  // Remover el corazón después de la animación
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
 
 // Función de cuenta regresiva y revelación del contenido
 function countdown() {
   let count = 10;
   const countdownInterval = setInterval(() => {
     countdownNumber.textContent = count;
+    createHearts(); // Genera un corazón en cada segundo de cuenta regresiva
     count--;
 
     if (count === -1) {
